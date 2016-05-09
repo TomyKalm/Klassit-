5c97914ee7e0bd22d5333b1e21179319d2e61793
Karl <Karl.linke@hotmail.com>
2016-05-09 09:17:20 +0300

    Graafika pool lõpetatud

--- Kalkulaator/Kalkulaator/MainWindow.xaml.cs
+++ Kalkulaator/Kalkulaator/MainWindow.xaml.cs
@@ -25,19 +25,5 @@ namespace Kalkulaator
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("OH NOOOOO!");
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
